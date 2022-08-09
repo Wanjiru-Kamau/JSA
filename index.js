10ec3fecacc404b31a5ca951481c1d98c2447f07
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-08-09 18:03:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// On the contact us page, ask the user for the following details(5 marks):
+// Company Name
+// Phone number
+// Location
+// Booking dates -- i should choose date from a calendar
+// 1)
+// Display this information with the filled in details. I.e grab the user input then
+// display is on the screen(5 marks)
+var listing = document.createElement("Contact")
+var add = document.createTextNode("company name")
+var add = document.createTextNode("phone number")
+var add = document.createTextNode("location")
+var add = document.createTextNode("booking date")
+listing.appendChild(add)
+var lastList=document.getElementById("bookingList")
+lastList.appendChild(listing);
+
+
+//   <script>
+// Automatic Slideshow - change image every 3 seconds
+{/* <script></script>
+var myIndex = 0;
+carousel();
+</nav>
+function carousel() {
+<!-- Slide Show -->
+<section>
+<img class="mySlides" src="https://nairobinews.nation.co.ke/wp-content/uploads/2019/09/Sauti-Sol.jpg"
+var i;
+var x = document.getElementsByClassName("mySlides");
+for (i = 0; i < x.length; i++) {
+style="width:100%">
+<img class="mySlides" src="https://i2.wp.com/www.soundsofafrica.org/wp-content/uploads/2020/07/sauti-sol-2.jpg?fit=1200%2C1200&ssl=1"
+style="width:100%">
+<img class="mySlides" src="http://ocdn.eu/images/pulscms/NTE7MDA_/30b2539286250f255f865326b5d72f7e.png"
+style="width:100%">
+x[i].style.display = "none";
+}
+myIndex++;
+if (myIndex > x.length) {myIndex = 1}
+x[myIndex-1].style.display = "block"; */}
